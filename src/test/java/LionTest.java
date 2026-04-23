4924c5a99692bcb9dcc932454b8194fecd83ef0f
Anastasia Moskvina <moskvina_nastya95@mail.ru>
2026-04-23 14:51:29 +0300

    Add tests and jacoco report to develop

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -2,41 +2,16 @@ package com.example;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.mockito.Mockito;
 import java.util.List;
 
-@RunWith(Parameterized.class)
 public class LionTest {
-
-    private final String sex;
-    private final boolean expectedHasMane;
-
-    public LionTest(String sex, boolean expectedHasMane) {
-        this.sex = sex;
-        this.expectedHasMane = expectedHasMane;
-    }
-
-    @Parameterized.Parameters
-    public static Object[][] getTestData() {
-        return new Object[][]{
-                {"Самец", true},
-                {"Самка", false},
-        };
-    }
-
-    @Test
-    public void shouldHaveCorrectManeStatus() throws Exception {
-        Feline feline = Mockito.mock(Feline.class);
-        Lion lion = new Lion(sex, feline);
-        Assert.assertEquals(expectedHasMane, lion.doesHaveMane());
-    }
 
     @Test
     public void getKittensReturnsCorrectValue() throws Exception {
         Feline feline = Mockito.mock(Feline.class);
-        Lion lion = new Lion(sex, feline); // используем текущий пол из параметров
+        // Передаем любой корректный пол, так как метод не зависит от него
+        Lion lion = new Lion("Самец", feline);
         Mockito.when(feline.getKittens()).thenReturn(1);
         Assert.assertEquals(1, lion.getKittens());
     }
@@ -44,9 +19,15 @@ public class LionTest {
     @Test
     public void getFoodReturnsCorrectList() throws Exception {
         Feline feline = Mockito.mock(Feline.class);
-        Lion lion = new Lion(sex, feline); // используем текущий пол из параметров
+        Lion lion = new Lion("Самка", feline);
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
         Mockito.when(feline.getFood("Хищник")).thenReturn(expectedFood);
         Assert.assertEquals(expectedFood, lion.getFood());
+    }
+
+    @Test(expected = Exception.class)
+    public void constructorThrowsExceptionOnInvalidSex() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+        new Lion("Некорректный пол", feline);
     }
 }
